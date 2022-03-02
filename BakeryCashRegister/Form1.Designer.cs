ea8f71572ec7f118f36264a6860ad3bce453a1db
Matilda Sarichith <matisari676@ed.amdsb.ca>
2022-03-02 13:33:16 -0500

    continued project

--- BakeryCashRegister/Form1.Designer.cs
+++ BakeryCashRegister/Form1.Designer.cs
@@ -40,49 +40,49 @@
             this.taxLabel = new System.Windows.Forms.Label();
             this.totalLabel = new System.Windows.Forms.Label();
             this.tenderedLabel = new System.Windows.Forms.Label();
-            this.tenderedOutput = new System.Windows.Forms.TextBox();
+            this.tenderedInput = new System.Windows.Forms.TextBox();
             this.changeButton = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.reciptButton = new System.Windows.Forms.Button();
             this.newOrderButton = new System.Windows.Forms.Button();
             this.reciptBaseLabel = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.bannerLabel = new System.Windows.Forms.Label();
-            this.titleLabel = new System.Windows.Forms.Label();
             this.subtotalOutput = new System.Windows.Forms.Label();
             this.taxOutput = new System.Windows.Forms.Label();
             this.totalOutput = new System.Windows.Forms.Label();
             this.changeOutput = new System.Windows.Forms.Label();
             this.reciptDescription = new System.Windows.Forms.Label();
+            this.bannerLabel = new System.Windows.Forms.Label();
+            this.titleLabel = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // numOfCupcakeLabel
             // 
             this.numOfCupcakeLabel.AutoSize = true;
-            this.numOfCupcakeLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.numOfCupcakeLabel.Font = new System.Drawing.Font("Rockwell", 10F);
             this.numOfCupcakeLabel.Location = new System.Drawing.Point(32, 58);
             this.numOfCupcakeLabel.Name = "numOfCupcakeLabel";
-            this.numOfCupcakeLabel.Size = new System.Drawing.Size(138, 17);
+            this.numOfCupcakeLabel.Size = new System.Drawing.Size(141, 17);
             this.numOfCupcakeLabel.TabIndex = 0;
             this.numOfCupcakeLabel.Text = "Number of cupcakes";
             // 
             // numOfCakeLabel
             // 
             this.numOfCakeLabel.AutoSize = true;
-            this.numOfCakeLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.numOfCakeLabel.Font = new System.Drawing.Font("Rockwell", 10F);
             this.numOfCakeLabel.Location = new System.Drawing.Point(32, 98);
             this.numOfCakeLabel.Name = "numOfCakeLabel";
-            this.numOfCakeLabel.Size = new System.Drawing.Size(115, 17);
+            this.numOfCakeLabel.Size = new System.Drawing.Size(117, 17);
             this.numOfCakeLabel.TabIndex = 1;
             this.numOfCakeLabel.Text = "Number of cakes";
             // 
             // numOfCookiesLabel
             // 
             this.numOfCookiesLabel.AutoSize = true;
-            this.numOfCookiesLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.numOfCookiesLabel.Font = new System.Drawing.Font("Rockwell", 10F);
             this.numOfCookiesLabel.Location = new System.Drawing.Point(32, 138);
             this.numOfCookiesLabel.Name = "numOfCookiesLabel";
-            this.numOfCookiesLabel.Size = new System.Drawing.Size(126, 17);
+            this.numOfCookiesLabel.Size = new System.Drawing.Size(130, 17);
             this.numOfCookiesLabel.TabIndex = 2;
             this.numOfCookiesLabel.Text = "Number of cookies";
             // 
@@ -123,17 +123,17 @@
             // subTotalLabel
             // 
             this.subTotalLabel.AutoSize = true;
-            this.subTotalLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.subTotalLabel.Font = new System.Drawing.Font("Rockwell", 10F);
             this.subTotalLabel.Location = new System.Drawing.Point(32, 230);
             this.subTotalLabel.Name = "subTotalLabel";
-            this.subTotalLabel.Size = new System.Drawing.Size(69, 17);
+            this.subTotalLabel.Size = new System.Drawing.Size(67, 17);
             this.subTotalLabel.TabIndex = 7;
             this.subTotalLabel.Text = "Sub Total";
             // 
             // taxLabel
             // 
             this.taxLabel.AutoSize = true;
-            this.taxLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.taxLabel.Font = new System.Drawing.Font("Rockwell", 10F);
             this.taxLabel.Location = new System.Drawing.Point(32, 262);
             this.taxLabel.Name = "taxLabel";
             this.taxLabel.Size = new System.Drawing.Size(31, 17);
@@ -143,29 +143,29 @@
             // totalLabel
             // 
             this.totalLabel.AutoSize = true;
-            this.totalLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.totalLabel.Font = new System.Drawing.Font("Rockwell", 10F);
             this.totalLabel.Location = new System.Drawing.Point(32, 296);
             this.totalLabel.Name = "totalLabel";
-            this.totalLabel.Size = new System.Drawing.Size(40, 17);
+            this.totalLabel.Size = new System.Drawing.Size(39, 17);
             this.totalLabel.TabIndex = 9;
             this.totalLabel.Text = "Total";
             // 
             // tenderedLabel
             // 
             this.tenderedLabel.AutoSize = true;
-            this.tenderedLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.tenderedLabel.Font = new System.Drawing.Font("Rockwell", 10F);
             this.tenderedLabel.Location = new System.Drawing.Point(32, 327);
             this.tenderedLabel.Name = "tenderedLabel";
-            this.tenderedLabel.Size = new System.Drawing.Size(70, 17);
+            this.tenderedLabel.Size = new System.Drawing.Size(72, 17);
             this.tenderedLabel.TabIndex = 10;
             this.tenderedLabel.Text = "Tendered";
             // 
-            // tenderedOutput
+            // tenderedInput
             // 
-            this.tenderedOutput.Location = new System.Drawing.Point(176, 327);
-            this.tenderedOutput.Name = "tenderedOutput";
-            this.tenderedOutput.Size = new System.Drawing.Size(68, 20);
-            this.tenderedOutput.TabIndex = 11;
+            this.tenderedInput.Location = new System.Drawing.Point(176, 327);
+            this.tenderedInput.Name = "tenderedInput";
+            this.tenderedInput.Size = new System.Drawing.Size(68, 20);
+            this.tenderedInput.TabIndex = 11;
             // 
             // changeButton
             // 
@@ -183,10 +183,10 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label1.Font = new System.Drawing.Font("Rockwell", 10F);
             this.label1.Location = new System.Drawing.Point(32, 416);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(57, 17);
+            this.label1.Size = new System.Drawing.Size(59, 17);
             this.label1.TabIndex = 13;
             this.label1.Text = "Change";
             // 
@@ -214,6 +214,7 @@
             this.newOrderButton.TabIndex = 15;
             this.newOrderButton.Text = "New Order";
             this.newOrderButton.UseVisualStyleBackColor = false;
+            this.newOrderButton.Click += new System.EventHandler(this.newOrderButton_Click);
             // 
             // reciptBaseLabel
             // 
@@ -231,25 +232,6 @@
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(0, 17);
             this.label2.TabIndex = 17;
-            // 
-            // bannerLabel
-            // 
-            this.bannerLabel.BackColor = System.Drawing.Color.Crimson;
-            this.bannerLabel.ForeColor = System.Drawing.Color.White;
-            this.bannerLabel.Location = new System.Drawing.Point(-5, -4);
-            this.bannerLabel.Name = "bannerLabel";
-            this.bannerLabel.Size = new System.Drawing.Size(785, 38);
-            this.bannerLabel.TabIndex = 18;
-            // 
-            // titleLabel
-            // 
-            this.titleLabel.BackColor = System.Drawing.Color.Crimson;
-            this.titleLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
-            this.titleLabel.ForeColor = System.Drawing.Color.White;
-            this.titleLabel.Location = new System.Drawing.Point(322, 9);
-            this.titleLabel.Name = "titleLabel";
-            this.titleLabel.Size = new System.Drawing.Size(100, 23);
-            this.titleLabel.TabIndex = 19;
             // 
             // subtotalOutput
             // 
@@ -292,26 +274,46 @@
             this.reciptDescription.Size = new System.Drawing.Size(300, 317);
             this.reciptDescription.TabIndex = 24;
             // 
+            // bannerLabel
+            // 
+            this.bannerLabel.BackColor = System.Drawing.Color.Crimson;
+            this.bannerLabel.Location = new System.Drawing.Point(0, 0);
+            this.bannerLabel.Name = "bannerLabel";
+            this.bannerLabel.Size = new System.Drawing.Size(779, 43);
+            this.bannerLabel.TabIndex = 25;
+            // 
+            // titleLabel
+            // 
+            this.titleLabel.AutoSize = true;
+            this.titleLabel.BackColor = System.Drawing.Color.Crimson;
+            this.titleLabel.Font = new System.Drawing.Font("Cooper Black", 20.5F);
+            this.titleLabel.ForeColor = System.Drawing.Color.White;
+            this.titleLabel.Location = new System.Drawing.Point(265, 9);
+            this.titleLabel.Name = "titleLabel";
+            this.titleLabel.Size = new System.Drawing.Size(251, 32);
+            this.titleLabel.TabIndex = 26;
+            this.titleLabel.Text = "Cookies n\' Cakes";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
             this.ClientSize = new System.Drawing.Size(777, 487);
+            this.Controls.Add(this.titleLabel);
+            this.Controls.Add(this.bannerLabel);
             this.Controls.Add(this.reciptDescription);
             this.Controls.Add(this.changeOutput);
             this.Controls.Add(this.totalOutput);
             this.Controls.Add(this.taxOutput);
             this.Controls.Add(this.subtotalOutput);
-            this.Controls.Add(this.titleLabel);
-            this.Controls.Add(this.bannerLabel);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.reciptBaseLabel);
             this.Controls.Add(this.newOrderButton);
             this.Controls.Add(this.reciptButton);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.changeButton);
-            this.Controls.Add(this.tenderedOutput);
+            this.Controls.Add(this.tenderedInput);
             this.Controls.Add(this.tenderedLabel);
             this.Controls.Add(this.totalLabel);
             this.Controls.Add(this.taxLabel);
@@ -344,20 +346,20 @@
         private System.Windows.Forms.Label taxLabel;
         private System.Windows.Forms.Label totalLabel;
         private System.Windows.Forms.Label tenderedLabel;
-        private System.Windows.Forms.TextBox tenderedOutput;
+        private System.Windows.Forms.TextBox tenderedInput;
         private System.Windows.Forms.Button changeButton;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button reciptButton;
         private System.Windows.Forms.Button newOrderButton;
         private System.Windows.Forms.Label reciptBaseLabel;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label bannerLabel;
-        private System.Windows.Forms.Label titleLabel;
         private System.Windows.Forms.Label subtotalOutput;
         private System.Windows.Forms.Label taxOutput;
         private System.Windows.Forms.Label totalOutput;
         private System.Windows.Forms.Label changeOutput;
         private System.Windows.Forms.Label reciptDescription;
+        private System.Windows.Forms.Label bannerLabel;
+        private System.Windows.Forms.Label titleLabel;
     }
 }
 
--- BakeryCashRegister/Form1.cs
+++ BakeryCashRegister/Form1.cs
@@ -53,18 +53,54 @@ namespace BakeryCashRegister
             catch
             {
                 subtotalOutput.Text = "Error";
+                taxOutput.Text = "Error";
+                totalOutput.Text = "Error";
             }
         }
 
         private void changeButton_Click(object sender, EventArgs e)
         {
+            tendered = Convert.ToInt16(tenderedInput.Text);
+            
             change = tendered - totalCost;
             changeOutput.Text = $"{change.ToString("C")}";
         }
 
         private void reciptButton_Click(object sender, EventArgs e)
         {
-            reciptDescription.Text
+            reciptDescription.Text = "Cookies n' Cakes";
+
+            reciptDescription.Text += "\nOrder number 45";
+            reciptDescription.Text += "\n\nMarch 1, 2022";
+
+            reciptDescription.Text += $"\n\nCupcakes x{numOfCupcakes} @ {cupcakeCost.ToString ("C")}";
+            reciptDescription.Text += $"\nCakes x{numOfCakes} @ {cakeCost.ToString("C")}";
+            reciptDescription.Text += $"\nCookies x{numOfCookies} @ {cookieCost.ToString("C")}";
+
+            reciptDescription.Text += $"\n\nSubtotal {subTotal.ToString("C")}";
+            reciptDescription.Text += $"\nTax {taxAmount.ToString("C")}";
+            reciptDescription.Text += $"\nTotal {totalCost.ToString("C")}";
+
+            reciptDescription.Text += $"\n\nTendered {tendered.ToString("C")}";
+            reciptDescription.Text += $"\nChange {change.ToString("C")}";
+
+            reciptDescription.Text += "\n\nHave a nice day!!";
+        }
+
+        private void newOrderButton_Click(object sender, EventArgs e)
+        {
+            cupcakeInput.Text = "";
+            cakeInput.Text = "";
+            cookieInput.Text = "";
+
+            subtotalOutput.Text = "";
+            taxOutput.Text = "";
+            totalOutput.Text = "";
+            tenderedInput.Text = "";
+
+            changeOutput.Text = "";
+            reciptDescription.Text = "";
+
         }
     }
 }
